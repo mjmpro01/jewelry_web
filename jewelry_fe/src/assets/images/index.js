13e7472a456267ca4ccfcf7d00e6a914f564683c
phucdinhh <phuc.dinhngoc.7@gmail.com>
2024-11-17 11:16:20 +0700

    add home banner

--- jewelry_fe/src/assets/images/index.js
+++ jewelry_fe/src/assets/images/index.js
@@ -3,6 +3,7 @@ import homeBanner1 from "./home-banner-1.jpg";
 import homeBanner2 from "./home-banner-2.jpg";
 import homeBanner3 from "./home-banner-3.jpg";
 import homeBanner4 from "./home-banner-4.jpg";
+import homeBanner5 from "./home-banner-5.jpg";
 import messenger from "./messenger.png";
 import phone from "./phone.png";
 import zalo from "./zalo-icon.png";
@@ -30,4 +31,5 @@ export const images = {
   miniBanner1,
   miniBanner2,
   miniBanner3,
+  homeBanner5,
 };
